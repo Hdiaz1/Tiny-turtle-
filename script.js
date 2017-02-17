b3ca5136c8ba0deff58e38f0d40daf999e7d262a
Hillary Diaz <hdiaz@cristoreyny.org>
2017-02-17 14:47:49 +0000

    something square

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-// JavaScript File
+// Tiny Turtle Setup. Avoid modifying these lines of code!
+var adjCanvasSize = document.getElementsByTagName('canvas')[0];
+adjCanvasSize.width  = 400;
+adjCanvasSize.height = 400;
+TinyTurtle.apply(window);
+
+// Start writing code here
+
+
+function square(width) {
+    forward(width);
+left(90);
+forward(width);
+left(90);
+forward(width);
+left(90);
+forward(width);
+
+  } 
+  square(200);
+  stamp();
+  
